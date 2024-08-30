b5e25e5a3c32fdcb67490cb6d46c8d4124cfab63
unknown <josea.hernandezl@uqvirtual.edu.co>
2024-08-29 19:11:14 -0500

    esto es un a prueba

---
+++ src/cuadrilatero.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class cuadrilatero {
+
+	public static void main(String[] agrs) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce el numero para la base: ");
+		int base = entrada.nextInt();
+		System.out.println("Introduce el numero para la altura: ");
+		int altura = entrada.nextInt();
+		
+		for (int i=0; i<altura; i++) {
+			for (int j=0; j<base; j++) {
+				if (i == 0 || i == altura-1) {
+					System.out.print("*");
+				}
+				else if (j == 0 || j == base-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		entrada.close();
+	}
+}
